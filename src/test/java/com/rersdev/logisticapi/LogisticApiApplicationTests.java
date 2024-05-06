2677af24af2c0a13c5f7fb6c9e55fef6018dd3aa
rodis2003 <rodisenrique73@gmail.com>
2024-05-06 15:48:09 -0500

    initial project

---
+++ src/test/java/com/rersdev/logisticapi/LogisticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rersdev.logisticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
